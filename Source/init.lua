b486c57209077a4350c9816617416da62ad05c9c
Chris Brucks <cbrucks@gmail.com>
2016-02-14 00:45:59 -0600

    updates to the webserver that fix the serving of the webpage. also added a minified version of the webPage.html as index.html

--- Source/init.lua
+++ Source/init.lua
@@ -1,3 +1,11 @@
-weather = require("weather")
+cfg = {};
+cfg.ssid = "Tempescope" ..  node.chipid();
+cfg.pwd = "t" .. node.chipid();
+cfg.auth = AUTH_WPA2_PSK;
+cfg.hidden = 0;
+cfg.max = 1;
 
-weather.update();
+wifi.ap.config(cfg);
+wifi.setmode(wifi.STATIONAP);
+
+dofile("server.lua");
--- Source/server.lua
+++ Source/server.lua
@@ -1,13 +1,23 @@
 require("httpserver-request")
 
-srv=net.createServer(net.TCP) 
+srv=net.createServer(net.TCP)
 srv:listen(80, function(conn) 
     conn:on("receive", function(conn, payload)
-        print(payload) 
-        file.open("webpage.html", "r")
-        conn:send(file.read())
-        file.close()
-        conn:close()
+        --local req = dofile("httpserver-request")(payload)
+        --for key,value in pairs(req) do print(key,value) end;
+        
+        file.open("index.html", "r")
+        local function sender(conn)
+            local fileContents = file.read();
+            if (fileContents ~= nil) then
+                conn:send(fileContents);
+            else
+                file.close();
+                conn:close();
+            end
+        end
+        conn:on("sent", sender)
+        sender(conn)
 	end) 
 end)
           
