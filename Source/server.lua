9e6695486a45b043c280bfe006cfbe34f81212c0
Chris Brucks <cbrucks@gmail.com>
2016-02-15 23:09:42 -0600

    made changes to the served webpage

--- Source/server.lua
+++ Source/server.lua
@@ -1,12 +1,7 @@
-require("httpserver-request")
-
 srv=net.createServer(net.TCP)
 srv:listen(80, function(conn) 
     conn:on("receive", function(conn, payload)
-        --local req = dofile("httpserver-request")(payload)
-        --for key,value in pairs(req) do print(key,value) end;
-        
-        file.open("index.html", "r")
+        file.open("webPage.html", "r")
         local function sender(conn)
             local fileContents = file.read();
             if (fileContents ~= nil) then
@@ -14,6 +9,9 @@ srv:listen(80, function(conn)
             else
                 file.close();
                 conn:close();
+                
+                local req = dofile("httpserver-request.lua")(payload)
+                for key,value in pairs(req.uri.args) do print(key,value) end;
             end
         end
         conn:on("sent", sender)
